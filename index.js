6517abf30f58a951f488c472914dfe73f1c7c1ba
_Mads <75088349+TFSMads@users.noreply.github.com>
2024-02-19 21:19:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const fs = require('fs');
 const path = require('path');
 const app = express()
-const port = 3000
+const port = process.env.PORT != null ? process.env.PORT : 443
 
 app.use(express.json());
 
